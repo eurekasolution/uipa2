8cd8e05160e24f8a7c812580d48a4b3ca5dab5df
eurekasolution <kjmin@daum.net>
2023-08-21 17:23:49 +0900

    A1723

---
+++ html/03.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8" />
+        <title>HTML5 ❤😂</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="style01.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    </head>
+<body>
+    Web Program <br>
+
+    <?php
+        $i = 3;
+        $i ++;
+        echo "$i <br>";
+
+        $i = "홍길동";
+        echo "i = $i<br>";
+
+        for($i=1; $i<=10; $i++) 
+        {
+            echo "$i<br>";
+        }
+    ?>
+</body>
+</html>
---
+++ html/04.php
@@ -0,0 +1,5 @@
+<?php
+    $i = "UIPA";
+
+    echo "i = $i<br>";
+?>
